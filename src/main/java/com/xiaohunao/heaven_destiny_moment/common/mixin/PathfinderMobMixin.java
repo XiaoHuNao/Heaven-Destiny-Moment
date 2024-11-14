a1429e978e9cf35aeaba4d49095ef6ced0f3e527
westernat <westernat233@163.com>
2024-11-14 16:55:26 +0800

    服务端启动性修复

--- src/main/java/com/xiaohunao/heaven_destiny_moment/common/mixin/PathfinderMobMixin.java
+++ src/main/java/com/xiaohunao/heaven_destiny_moment/common/mixin/PathfinderMobMixin.java
@@ -8,7 +8,6 @@ import com.xiaohunao.heaven_destiny_moment.common.moment.MomentManager;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.PathfinderMob;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,15 +18,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class PathfinderMobMixin {
     @Inject(method = "checkSpawnRules", at = @At(value = "HEAD", ordinal = 0), remap = true, cancellable = true)
     public void checkSpawnRules(LevelAccessor pLevel, MobSpawnType pSpawnReason, CallbackInfoReturnable<Boolean> cir) {
-        if (pLevel.isClientSide()){
-            return;
-        }
+        if (!(pLevel instanceof ServerLevel serverLevel)) return;
 
-        MomentManager momentManager = MomentManager.of((ServerLevel) pLevel);
-        PathfinderMob mob = (PathfinderMob)(Object)this;
+        MomentManager momentManager = MomentManager.of(serverLevel);
+        PathfinderMob mob = (PathfinderMob) (Object) this;
         for (MomentInstance instance : momentManager.getRunMoments().values()) {
             instance.getMoment()
-                    .filter(moment -> moment.isInArea((ServerLevel) pLevel,mob.blockPosition()))
+                    .filter(moment -> moment.isInArea(serverLevel, mob.blockPosition()))
                     .map(Moment::getMomentDataContext)
                     .flatMap(MomentDataContext::mobSpawnSettingsContext)
                     .flatMap(MobSpawnSettingsContext::ignoreLightLevel)
