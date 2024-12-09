7687b52103cdefc61e924df308564ea6f70cffe9
XiaoHuNao <shiwenbin2250@gmail.com>
2024-12-10 00:37:38 +0800

    删除CodecExtra

--- src/main/java/com/xiaohunao/heaven_destiny_moment/common/context/MomentDataContext.java
+++ src/main/java/com/xiaohunao/heaven_destiny_moment/common/context/MomentDataContext.java
@@ -2,23 +2,23 @@ package com.xiaohunao.heaven_destiny_moment.common.context;
 
 
 import com.google.common.base.Function;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import com.xiaohunao.heaven_destiny_moment.common.codec.CodecExtra;
 import com.xiaohunao.heaven_destiny_moment.common.context.condition.IConditionContext;
 import com.xiaohunao.heaven_destiny_moment.common.context.reward.IRewardContext;
 
 import java.util.*;
 import java.util.function.Consumer;
 
-public record MomentDataContext(Optional<Integer> readyTime, Optional<Set<IRewardContext>> rewards, Optional<Set<IConditionContext>> conditions,
+public record MomentDataContext(Optional<Integer> readyTime, Optional<HashSet<IRewardContext>> rewards, Optional<HashSet<IConditionContext>> conditions,
                                 Optional<EntitySpawnSettingsContext> entitySpawnSettingsContext) {
 
     public static final Codec<MomentDataContext> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             Codec.INT.optionalFieldOf("ready_time").forGetter(MomentDataContext::readyTime),
-            CodecExtra.setOf(IRewardContext.CODEC).optionalFieldOf("rewards").forGetter(MomentDataContext::rewards),
-            CodecExtra.setOf(IConditionContext.CODEC).optionalFieldOf("conditions").forGetter(MomentDataContext::conditions),
+            Codec.list(IRewardContext.CODEC).xmap(Sets::newHashSet, Lists::newArrayList).optionalFieldOf("rewards").forGetter(MomentDataContext::rewards),
+            Codec.list(IConditionContext.CODEC).xmap(Sets::newHashSet, Lists::newArrayList).optionalFieldOf("conditions").forGetter(MomentDataContext::conditions),
             EntitySpawnSettingsContext.CODEC.optionalFieldOf("entity_spawn_settings").forGetter(MomentDataContext::entitySpawnSettingsContext)
     ).apply(instance, MomentDataContext::new));
     public static final MomentDataContext EMPTY = new MomentDataContext(Optional.empty(),Optional.empty(),Optional.empty(),Optional.empty());
@@ -26,8 +26,8 @@ public record MomentDataContext(Optional<Integer> readyTime, Optional<Set<IRewar
 
     public static class Builder {
         private Integer readyTime;
-        private Set<IRewardContext> rewards;
-        private Set<IConditionContext> conditions;
+        private HashSet<IRewardContext> rewards;
+        private HashSet<IConditionContext> conditions;
         private EntitySpawnSettingsContext mobSpawnSettingsContext;
 
 
