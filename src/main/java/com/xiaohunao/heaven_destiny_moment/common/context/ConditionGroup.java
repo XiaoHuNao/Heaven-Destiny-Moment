a8db016e3ed4e00655d10a923e5137780c8063d5
westernat <westernat233@163.com>
2024-12-13 18:21:25 +0800

    改粒子风暴，修泰拉事件

--- src/main/java/com/xiaohunao/heaven_destiny_moment/common/context/ConditionGroup.java
+++ src/main/java/com/xiaohunao/heaven_destiny_moment/common/context/ConditionGroup.java
@@ -1,8 +1,6 @@
 package com.xiaohunao.heaven_destiny_moment.common.context;
 
 import com.google.common.collect.Lists;
-
-import com.google.common.collect.Maps;
 import com.mojang.datafixers.util.Pair;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
@@ -10,9 +8,14 @@ import com.xiaohunao.heaven_destiny_moment.common.context.condition.ICondition;
 
 import java.util.List;
 import java.util.Optional;
-public record ConditionGroup(Optional<Pair<Boolean,List<ICondition>>> create , Optional<List<ICondition>> victory , Optional<List<ICondition>> lose , Optional<List<ICondition>> end) {
+
+public record ConditionGroup(Optional<Pair<Boolean, List<ICondition>>> create, Optional<List<ICondition>> victory, Optional<List<ICondition>> lose, Optional<List<ICondition>> end) {
+    public static final Codec<Pair<Boolean, List<ICondition>>> CREATE_CODEC = RecordCodecBuilder.create(instance -> instance.group(
+            Codec.BOOL.lenientOptionalFieldOf("auto", true).forGetter(Pair::getFirst),
+            Codec.list(ICondition.CODEC).fieldOf("conditions").orElseGet(List::of).forGetter(Pair::getSecond)
+    ).apply(instance, Pair::new));
     public static final Codec<ConditionGroup> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-            Codec.pair(Codec.BOOL,Codec.list(ICondition.CODEC)).optionalFieldOf("create").forGetter(ConditionGroup::create),
+            CREATE_CODEC.optionalFieldOf("create").forGetter(ConditionGroup::create),
             Codec.list(ICondition.CODEC).optionalFieldOf("victory").forGetter(ConditionGroup::victory),
             Codec.list(ICondition.CODEC).optionalFieldOf("lose").forGetter(ConditionGroup::lose),
             Codec.list(ICondition.CODEC).optionalFieldOf("end").forGetter(ConditionGroup::end)
@@ -20,7 +23,7 @@ public record ConditionGroup(Optional<Pair<Boolean,List<ICondition>>> create , O
 
 
     public static class Builder {
-        private Pair<Boolean,List<ICondition>> create;
+        private Pair<Boolean, List<ICondition>> create;
         private List<ICondition> victory;
         private List<ICondition> lose;
         private List<ICondition> end;
@@ -29,29 +32,30 @@ public record ConditionGroup(Optional<Pair<Boolean,List<ICondition>>> create , O
             return new ConditionGroup(Optional.ofNullable(create), Optional.ofNullable(victory), Optional.ofNullable(lose), Optional.ofNullable(end));
         }
 
-        public Builder create(boolean auto,ICondition... conditions){
-            if (this.create == null){
+        public Builder create(boolean auto, ICondition... conditions) {
+            if (this.create == null) {
                 this.create = Pair.of(auto, List.of(conditions));
             }
             return this;
         }
 
-        public Builder victory(ICondition... conditions){
-            this.victory = add(this.victory,conditions);
+        public Builder victory(ICondition... conditions) {
+            this.victory = add(this.victory, conditions);
             return this;
         }
 
-        public Builder lose(ICondition... conditions){
-            this.lose = add(this.lose,conditions);
+        public Builder lose(ICondition... conditions) {
+            this.lose = add(this.lose, conditions);
             return this;
         }
 
-        public Builder end(ICondition... conditions){
-            this.end = add(this.end,conditions);
+        public Builder end(ICondition... conditions) {
+            this.end = add(this.end, conditions);
             return this;
         }
-        private List<ICondition> add(List<ICondition> conditions, ICondition... condition){
-            if (conditions == null){
+
+        private List<ICondition> add(List<ICondition> conditions, ICondition... condition) {
+            if (conditions == null) {
                 conditions = Lists.newArrayList();
             }
             conditions.addAll(List.of(condition));
